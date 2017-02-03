856a2f0b0284810a0d43677f97fd90f95c3a18b1
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-02-03 21:54:41 +0900

    createjs with click event

--- src/js/2017/02/canvas.js
+++ src/js/2017/02/canvas.js
@@ -1,29 +1,51 @@
 import $ from "jquery";
 import EventEmitter from "events";
+const IMAGES = [
+  '/images/2017/02/image1.jpg',
+  '/images/2017/02/image2.jpg',
+  '/images/2017/02/image3.jpg',
+  '/images/2017/02/image4.jpg'
+];
 
 const FRAMES = {};
-const DEFAULT = "http://placehold.it/300x200/719b3b/fff.jpg/?text=Default";
 
 class Canvas extends EventEmitter {
   constructor(opts={}){
     super();
+    console.log(createjs);
     this.$canvas   = opts.$canvas;
     this.canvasCxt = opts.$canvas[0].getContext('2d');
+    this.stage     = new createjs.Stage('canvas');
 
     this.init();
   }
 
   init() {
-    const defaultImage= new Image();
-    defaultImage.src = DEFAULT;
+    this.$canvas[0].width = this.$canvas.width();
+    this.$canvas[0].height = this.$canvas.height();
+    this.stage.enableMouseOver();
 
-    defaultImage.onload = () => {
-      console.log(defaultImage);
-      this.canvasCxt.drawImage(defaultImage, 0, 0);
-    };
+    let positionX = 0;
+    IMAGES.forEach((v,i)=>{
+      const bitmap = new createjs.Bitmap(v);
+      bitmap.cursor = 'pointer';
+      bitmap.addEventListener('click',()=>{ alert('click'); });
+      this.stage.addChild(bitmap);
+
+      if((i+1)%4==0){
+        positionX = 0;
+        bitmap.y  = 200;
+      }
+      bitmap.x = positionX*200;
+      positionX += 1;
+    });
+
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', ()=>{
+      this.stage.update();
+    });
   };
 }
-
 
 (()=>{
   const canvas = new Canvas({
