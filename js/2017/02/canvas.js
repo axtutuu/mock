26f91683b780bb2b979ee4e86a6595d51db8b6d3
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-02-03 21:58:08 +0900

    !　

--- js/2017/02/canvas.js
+++ js/2017/02/canvas.js
@@ -10139,7 +10139,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var IMAGES = ['/images/2017/02/image1.jpg', '/images/2017/02/image2.jpg', '/images/2017/02/image3.jpg', '/images/2017/02/image4.jpg'];
+var IMAGES = ['/mock/images/2017/02/image1.jpg', '/mock/images/2017/02/image2.jpg', '/mock/images/2017/02/image3.jpg', '/mock/images/2017/02/image4.jpg'];
 
 var FRAMES = {};
 
