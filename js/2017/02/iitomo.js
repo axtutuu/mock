f757aeff8738390558efb38dc293b83ddb1015bd
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-02-20 00:06:42 +0900

    flow base

--- js/2017/02/iitomo.js
+++ js/2017/02/iitomo.js
@@ -5,9 +5,44 @@
   var input = doc.querySelector('.js-image-input');
   var preview = doc.querySelector('.js-preview');
 
-  Promise.resolve().then(imageReader).then(faceTracker).then(function () {
+  var canvas = document.getElementById('canvas');
+  var canvasCxt = canvas.getContext('2d');
+
+  Promise.resolve().then(baseImage).then(imageReader).then(faceTracker).then(function () {
     console.log('next');
   });
+
+  function baseTracker() {
+    return new Promise(function (resolve) {
+      var tracker = new tracking.ObjectTracker(['face']);
+      tracking.track('#canvas', tracker);
+      tracker.on('track', function (e) {
+        var rect = e.data[0];
+        console.log(rect);
+        resolve();
+      });
+    });
+  }
+
+  function baseImage() {
+    var img = new Image();
+    return new Promise(function (resolve) {
+      img.src = "/images/2017/02/sample2.PNG";
+      img.onload = function () {
+        console.log(img);
+        console.log(img.width);
+        canvasCxt.drawImage(img, 0, 0, img.width, img.height);
+
+        var tracker = new tracking.ObjectTracker(['face']);
+        tracking.track(img, tracker);
+        tracker.on('track', function (e) {
+          var rect = e.data[0];
+          console.log(rect);
+          resolve();
+        });
+      };
+    });
+  }
 
   function imageReader() {
     return new Promise(function (resolve) {
@@ -31,56 +66,25 @@
       var tracker = new tracking.ObjectTracker(['face']);
       tracking.track('.js-preview', tracker);
       tracker.on('track', function (e) {
-        var canvas = document.getElementById('canvas');
-        var canvasCxt = canvas.getContext('2d');
+        var rect = e.data[0];
+        console.log(rect);
 
-        canvasCxt.beginPath();
-        canvasCxt.arc(65, 65, 45, 0, Math.PI * 2, false);
-        canvasCxt.clip();
+        var face = document.createElement('canvas');
+        var faceCxt = canvas.getContext('2d');
+
+        faceCxt.beginPath();
+        faceCxt.arc(65, 65, 45, 0, Math.PI * 2, false);
+        faceCxt.clip();
 
         var img = new Image();
         img.src = preview.src;
         img.onload = function () {
-          canvasCxt.drawImage(img, 98, 187, 90, 90, 20, 20, 90, 90);
+          faceCxt.drawImage(img, 86, 99, 119, 119, 20, 20, 90, 90);
           resolve();
         };
       });
     });
   }
-
-  input.addEventListener('change', function (e) {
-    console.log(e);
-    console.log(e.target.files);
-    preview.file = e.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function (res) {
-      console.log(res);
-      preview.src = res.target.result;
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  });
 })(window, document);
-
-// window.onload = function() {
-//   var img = document.getElementById('img');
-//   var tracker = new tracking.ObjectTracker(['face']);
-//   tracking.track('#img', tracker);
-//   tracker.on('track', function(event) {
-//     event.data.forEach(function(rect) {
-//       const canvas = document.getElementById('canvas');
-//       const canvasCxt = canvas.getContext('2d');
-//       canvasCxt.beginPath();
-//       canvasCxt.arc(65, 65, 45, 0, Math.PI*2, false);
-//       canvasCxt.clip();
-// 
-//       var img4 = new Image();
-//       img4.onload = function() {
-//         canvasCxt.drawImage(img4, 98, 187, 90, 90, 20, 20, 90, 90);
-//       };
-//       img4.src = img.src;
-//       console.log(rect);
-//     });
-//   });
-// };
 
 },{}]},{},[1]);
