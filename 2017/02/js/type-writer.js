c263c0cf8427eab5c0d72b643538f7d331d94294
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-02-26 14:06:58 +0900

    deploy

--- 2017/02/js/type-writer.js
+++ 2017/02/js/type-writer.js
@@ -2,19 +2,7 @@
 'use strict';
 
 (function () {
-
   $('#target').t();
-  // const text = document.querySelector('.text');
-  // const template = new t("<div>Hello {{=name}}</div>");
-  // // text.innerHTML = template.render({name: "World!"});
-  // console.log(template);
-  // text.innerHTML = template.render({name: "World!"});
 })();
-
-// const text = document.querySelector('.text');
-// text.innerHTML = "<p>hoge</p>";
-// 
-// var elem = document.getElementById("test");
-// elem.innerHTML = "<span style='color: red;'>span要素に変更したよ！</span>";
 
 },{}]},{},[1]);
