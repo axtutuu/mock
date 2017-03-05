9f9bfe11ca0ec4331bdf6fe48b6c59ec9df40dd7
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-03-05 19:54:05 +0900

    カスタマイズ cosor

--- src/2017/03/src/js/fabric.js
+++ src/2017/03/src/js/fabric.js
@@ -1,18 +1,42 @@
-const fabric = require('fabric').fabric;
-
-console.log(fabric);
-
 ((doc, win)=>{
   const canvas = doc.querySelector('.js-canvas');
   const stage  = new fabric.Canvas(canvas);
 
   const rect  = new fabric.Rect({
-    left: 100,
-    top:  100,
+    left: 0,
+    top:  0,
     fill: 'red',
-    width: 20,
-    height: 20
+    width: 100,
+    height: 100 
   });
 
+fabric.Object.prototype.customiseCornerIcons({
+    settings: {
+        borderColor: 'black',
+        cornerSize: 25,
+        cornerShape: 'rect',
+        cornerBackgroundColor: 'black',
+        cornerPadding: 10
+    },
+    tl: {
+        icon: 'images/Synchronize-100.png'
+    },
+    tr: {
+        icon: 'icons/resize.svg'
+    },
+    bl: {
+        icon: 'icons/remove.svg'
+    },
+    br: {
+        icon: 'icons/up.svg'
+    },
+    mb: {
+        icon: 'icons/down.svg'
+    }
+}, function() {
+    canvas.renderAll();
+} );
+
   stage.add(rect);
+
 })(document, window);
