fc6c2741b2e499e8358956c924ea105bef86a024
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-06-08 20:44:52 +0900

    build

--- 2017/06/js/08.js
+++ 2017/06/js/08.js
@@ -4,6 +4,95 @@
 var canvas = document.getElementById('canvas'),
     gl = canvas.getContext('webgl');
 
-canvas.width = 512, canvas.height = 512, function create_program() {};
+init();
+
+function init() {
+  var c = canvas;
+  canvas.width = 300, canvas.height = 300,
+
+  // 初期化
+  gl.clearColor(0, 0, 0, 1);
+  gl.clearDepth(1, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var vShader = create_shader('vs'),
+      fShader = create_shader('fs'),
+      pg = create_program(vShader, fShader),
+      attLocation = gl.getAttribLocation(pg, 'position'),
+      attStride = 3,
+      vertexPos = [0.0, 1.0, 0.0, 1, 0, 0.0, 0.0, -1.0, 0.0, 0.0],
+      vbo = create_vbo(vertexPos);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.enableVertexAttribArray(attLocation);
+  gl.vertexAttribPointer(attLocation, attStride, gl.FLOAT, false, 0, 0);
+
+  var m = new matIV(),
+      mMatrix = m.identity(m.create()),
+      vMatrix = m.identity(m.create()),
+      pMatrix = m.identity(m.create()),
+      mvpMatrix = m.identity(m.create());
+
+  m.lookAt([0.0, 1.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+
+  // 各行列を掛け合わせ座標変換行列を完成させる
+  m.multiply(pMatrix, vMatrix, mvpMatrix);
+  m.multiply(mvpMatrix, mMatrix, mvpMatrix);
+
+  var uniLocation = gl.getUniformLocation(pg, 'mvpMatrix');
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.flush();
+}
+
+function create_shader(id) {
+  var el = document.getElementById(id);
+  if (!el) return;
+
+  var shader = void 0;
+  switch (el.type) {
+    case 'x-shader/x-fragment':
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+    case 'x-shader/x-vertex':
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+  }
+
+  gl.shaderSource(shader, el.text);
+  gl.compileShader(shader);
+
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    return shader;
+  } else {
+    console.log('shader error', gl.getShaderInfoLog(shader));
+  }
+}
+
+function create_program(vs, fs) {
+  var pg = gl.createProgram();
+
+  gl.attachShader(pg, vs);
+  gl.attachShader(pg, fs);
+  gl.linkProgram(pg);
+
+  if (gl.getProgramParameter(pg, gl.LINK_STATUS)) {
+    gl.useProgram(pg);
+    return pg;
+  } else {
+    alert(gl.getProgramInfoLog(pg));
+  }
+}
+
+function create_vbo(data) {
+  var vbo = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  return vbo;
+}
 
 },{}]},{},[1]);
