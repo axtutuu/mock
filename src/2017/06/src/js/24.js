5765308763edc0e49a55508742571aae032a986a
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-06-24 21:30:21 +0900

    add vr manager

--- src/2017/06/src/js/24.js
+++ src/2017/06/src/js/24.js
@@ -1,3 +1,5 @@
+// require('webvr-boilerplate');
+import 'webvr-polyfill';
 /*
  * init setting
  */
@@ -9,6 +11,20 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 main.appendChild(renderer.domElement);
+
+/*
+ * vr setting
+ */
+const controls = new THREE.VRControls(camera);
+controls.standing = true;
+
+const effect = new THREE.VREffect(renderer);
+effect.setSize(window.innerWidth, window.innerHeight);
+
+const manager = new WebVRManager(renderer, effect);
+
+window.addEventListener('resize', onResize, true);
+window.addEventListener('vrdisplaypresentchange', onResize, true);
 
 /*
  * 箱をおく
@@ -23,7 +39,14 @@ function render() {
   cube.rotation.x += 0.1;
   cube.rotation.y += 0.1;
   requestAnimationFrame(render);
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+  manager.render(scene, camera);
+}
+
+function onResize(e) {
+  effect.setSize(window.innerWidth / window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 }
 render();
 // renderer.render(scene, camera);
