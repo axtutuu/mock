0c6c9633edabf4aeee3d672117c9dc76f552b216
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-08-17 23:11:59 +0900

    加速度

--- src/2017/08/src/js/17.js
+++ src/2017/08/src/js/17.js
@@ -1,1 +1,33 @@
-console.log('17');
+const canvas = document.querySelector('.canvas');
+const ctx    = canvas.getContext('2d');
+canvas.width = 600;
+canvas.height = 600;
+
+const velocityX = 5, accelY = 0.4;
+let velocityY = -20;
+
+let x = 0, y = 0;
+function tick() {
+  x += velocityX;
+  velocityY += accelY;
+  y += velocityY;
+
+  console.log(y, velocityY);
+
+  if(x > 600) x = 0;
+  if(y > 600) {
+    velocityY = -20;
+  }
+  paint(x, y);
+  requestAnimationFrame(tick);
+}
+
+function paint(x, y) {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, 600, 600);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(x, y, 10, 10);
+  ctx.fill();
+}
+tick();
+
