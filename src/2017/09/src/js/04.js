124317ffb7e54f973d6ccb884e8c8f3608d53632
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-09-05 00:35:45 +0900

    animatino

--- src/2017/09/src/js/04.js
+++ src/2017/09/src/js/04.js
@@ -6,21 +6,32 @@ ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 600, 600);
 
 
-// for(let i=0; i<10; i++) {
-//   ctx.fillRect((60*i), 0, 50, 50);
-//   for(let ii=0; ii<10; ii++) {
-//     ctx.fillRect((60*i), (60*ii), 50, 50);
-//   }
-// }
+let size = 0;
+let color = ['black', 'white'];
+let bg = color[0], box = color[1];
 
-let size = 20;
-ctx.fillStyle = 'black';
 function tick() {
-  size += 0.1;
+  ctx.fillStyle = bg;
+  ctx.fillRect(0, 0, 600, 600);
+
+  ctx.fillStyle = box;
+
+  size += 0.2;
   for(let i=0; i<10; i++) {
     ctx.fillRect((60*i), 0, size, size);
     for(let ii=0; ii<10; ii++) {
       ctx.fillRect((60*i), (60*ii), size, size);
+    }
+  }
+
+  if(size > 60) {
+    size = 0;
+    if(bg == 'black') {
+      bg = 'white';
+      box = 'black';
+    } else {
+      box = 'white';
+      bg = 'black';
     }
   }
 
