098233b52a542b00733bf9368f95cea312f2283e
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-09-06 09:25:59 +0900

    init

---
+++ src/2017/09/src/js/06.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('.canvas');
+const ctx    = canvas.getContext('2d');
+canvas.width = canvas.height = 600;
+
+ctx.fillStyle = '#d8d8d8';
+ctx.fillRect(0, 0, 600, 600);
