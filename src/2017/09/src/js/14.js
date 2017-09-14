e226aae0778ae0d944b480760ccd695d65e59e74
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-09-14 09:06:31 +0900

    scale取得

--- src/2017/09/src/js/14.js
+++ src/2017/09/src/js/14.js
@@ -1,2 +1,14 @@
-console.log($);
-console.log(14);
+const $width = $('.js-width');
+const $displayWidth = $('.js-display-width');
+const $scale = $('.js-sacle');
+
+const width = $(window).width();
+$(window).on('touchmove', function(evt) {
+  const innerWidth = window.innerWidth;
+
+  $width.text(width);
+  $displayWidth.text(innerWidth);
+  $scale.text(width/innerWidth);
+  console.log(evt.originalEvent.touches.length);
+  console.log(width/innerWidth, width, innerWidth);
+});
