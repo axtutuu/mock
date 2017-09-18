8bdaae7d2fa01f8676ca559174b9e565868637ba
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-09-18 18:23:47 +0900

    phase2

--- public/2017/09/js/18.js
+++ public/2017/09/js/18.js
@@ -5,6 +5,7 @@
 var main = {
   preload: function preload() {
     game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function create() {
@@ -16,10 +17,14 @@ var main = {
 
     var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+
+    // pipes
+    this.pipes = game.add.group();
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   },
 
   update: function update() {
-    if (this.bird.y < 0 || this.bird.y > 600) {
+    if (this.bird.y < 0 || this.bird.y > 490) {
       this.restartGame();
     }
   },
@@ -30,10 +35,33 @@ var main = {
 
   restartGame: function restartGame() {
     game.state.start('main');
+  },
+
+  addOnePipe: function addOnePipe(x, y) {
+    var pipe = game.add.sprite(x, y, 'pipe');
+    this.pipes.add(pipe);
+
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -200;
+
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function addRowOfPipes() {
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    console.log(hole);
+
+    for (var i = 0; i < 8; i++) {
+      if (i != hole && i != hole + 1) {
+        this.addOnePipe(400, i * 60 + 10);
+      }
+    }
   }
 };
 
-var game = new Phaser.Game(600, 600);
+var game = new Phaser.Game(400, 490);
 game.state.add('main', main);
 game.state.start('main');
 
--- src/2017/09/src/js/18.js
+++ src/2017/09/src/js/18.js
@@ -1,7 +1,8 @@
 // link: http://www.lessmilk.com/tutorial/flappy-bird-phaser-1
 const main = {
   preload: function() {
-    game.load.image('bird', 'assets/bird.png')
+    game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
   },
 
   create: function() {
@@ -13,10 +14,14 @@ const main = {
 
     const spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+
+    // pipes
+    this.pipes = game.add.group();
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   },
 
   update: function() {
-    if(this.bird.y < 0 || this.bird.y > 600) {
+    if(this.bird.y < 0 || this.bird.y > 490) {
       this.restartGame();
     }
   },
@@ -27,9 +32,32 @@ const main = {
 
   restartGame: function() {
     game.state.start('main');
+  },
+
+  addOnePipe: function(x, y) {
+    const pipe = game.add.sprite(x, y, 'pipe')
+    this.pipes.add(pipe);
+
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -200;
+
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  addRowOfPipes: function () {
+    const hole = Math.floor(Math.random()*5) + 1;
+
+    console.log(hole)
+
+    for(let i=0; i<8; i++) {
+        if(i != hole && i != hole+1) {
+          this.addOnePipe(400, i*60 + 10);
+        }
+    }
   }
 }
 
-const game = new Phaser.Game(600, 600);
+const game = new Phaser.Game(400, 490);
 game.state.add('main', main);
 game.state.start('main');
