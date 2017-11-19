888c132289a21d24dfff7f2649a47304123f3383
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-11-19 19:30:54 +0900

    get orientation function

--- public/2017/11/js/19.js
+++ public/2017/11/js/19.js
@@ -3,4 +3,36 @@
 
 console.log('image');
 
+/*
+ * get orientation
+ * https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side
+ */
+function getOrientation(base64) {
+  var view = new DataView(base64);
+  if (view.getUint16(0, false) != 0xFFD8) return -2;
+  var length = view.byteLength,
+      offset = 2;
+  while (offset < length) {
+    var marker = view.getUint16(offset, false);
+    offset += 2;
+    if (marker == 0xFFE1) {
+      if (view.getUint32(offset += 2, false) != 0x45786966) return -1;
+      var little = view.getUint16(offset += 6, false) == 0x4949;
+      offset += view.getUint32(offset + 4, little);
+      var tags = view.getUint16(offset, little);
+      offset += 2;
+      for (var i = 0; i < tags; i++) {
+        if (view.getUint16(offset + i * 12, little) == 0x0112) {
+          return view.getUint16(offset + i * 12 + 8, little);
+        }
+      }
+    } else if ((marker & 0xFF00) != 0xFF00) {
+      break;
+    } else {
+      offset += view.getUint16(offset, false);
+    }
+    return -1;
+  }
+}
+
 },{}]},{},[1]);
