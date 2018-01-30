906f94cec3d6bb8fa6b9133507c37cd83fc4c742
kawasaki-atsushi <kawasaki8910@gmail.com>
2018-01-30 12:21:35 +0900

    slot base

--- public/2018/01/js/29.js
+++ public/2018/01/js/29.js
@@ -1,4 +1,22 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
+
+var slot = document.querySelector('#slot1');
+var submit = document.querySelector('#submit');
+
+var pos = 0;
+var frameId = void 0;
+var roll = function roll() {
+  frameId = window.requestAnimationFrame(roll);
+  slot.style.transform = 'translateY(-' + pos + 'px)';
+  pos += 10;
+  if (pos > 525) pos = 0;
+};
+
+roll();
+
+submit.addEventListener('click', function () {
+  window.cancelAnimationFrame(frameId);
+});
 
 },{}]},{},[1]);
---
+++ src/2018/01/src/js/29.js
@@ -0,0 +1,17 @@
+const slot = document.querySelector('#slot1');
+const submit = document.querySelector('#submit');
+
+let pos = 0
+let frameId;
+const roll = () => {
+  frameId = window.requestAnimationFrame(roll)
+  slot.style.transform = `translateY(-${pos}px)`
+  pos += 10
+  if (pos > 525) pos = 0
+}
+
+roll()
+
+submit.addEventListener('click', () => {
+  window.cancelAnimationFrame(frameId);
+});
