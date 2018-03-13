35be986fb22d40b30f3ca679068b53e34348175b
kawasaki-atsushi <kawasaki8910@gmail.com>
2018-03-14 00:09:19 +0900

    画像の書き出し

--- public/2018/03/js/14.js
+++ public/2018/03/js/14.js
@@ -3,12 +3,14 @@
 
 var video = document.getElementById('video');
 var canvas = document.getElementById('canvas');
+var img = document.getElementById('img');
 var context = canvas.getContext('2d');
 var tracker = new tracking.ObjectTracker('face');
 tracker.setInitialScale(4);
 tracker.setStepSize(2);
 tracker.setEdgesDensity(0.1);
 tracking.track('#video', tracker, { camera: true });
+var rectObj = {};
 tracker.on('track', function (event) {
   context.clearRect(0, 0, canvas.width, canvas.height);
   event.data.forEach(function (rect) {
@@ -18,12 +20,18 @@ tracker.on('track', function (event) {
     context.fillStyle = "#fff";
     context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
     context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    rectObj = rect;
   });
 });
 
 canvas.addEventListener('click', function () {
-  var url = canvas.toDataURL('image/png');
-  console.log(url);
+  var c = document.createElement('canvas');
+  var x = c.getContext('2d');
+  c.width = 512;
+  c.height = 512;
+  x.clearRect(0, 0, c.width, c.height);
+  x.drawImage(video, rectObj.x + rectObj.width, rectObj.y + rectObj.height, rectObj.width * 2, rectObj.height * 2, 0, 125, 250, 250);
+  img.src = c.toDataURL('image/png');
 });
 
 },{}]},{},[1]);
