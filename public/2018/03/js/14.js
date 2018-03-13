2fb8f09340f168a086ea6a7bb0eb2d8aef60e30b
kawasaki-atsushi <kawasaki8910@gmail.com>
2018-03-13 22:06:44 +0900

    facetraking

---
+++ public/2018/03/js/14.js
@@ -0,0 +1,29 @@
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+'use strict';
+
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var tracker = new tracking.ObjectTracker('face');
+tracker.setInitialScale(4);
+tracker.setStepSize(2);
+tracker.setEdgesDensity(0.1);
+tracking.track('#video', tracker, { camera: true });
+tracker.on('track', function (event) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  event.data.forEach(function (rect) {
+    context.strokeStyle = '#a64ceb';
+    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    context.font = '11px Helvetica';
+    context.fillStyle = "#fff";
+    context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+    context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+  });
+});
+
+canvas.addEventListener('click', function () {
+  var url = canvas.toDataURL('image/png');
+  console.log(url);
+});
+
+},{}]},{},[1]);
