d552fc2826c4ed81739beaf81075249081003573
kawasaki-atsushi <kawasaki8910@gmail.com>
2018-04-07 15:59:52 +0900

    keyevent

--- public/2018/04/js/mario.js
+++ public/2018/04/js/mario.js
@@ -10,4 +10,31 @@ var lastAnimationFrameTime = 0;
 cxt.fillStyle = "rgb(255, 0, 0)";
 cxt.fillRect(0, 0, 640, 480);
 
+window.addEventListener('keydown', keyDown);
+window.addEventListener('keyup', keyUp);
+
+function keyDown(e) {
+  var code = e.keyCode;
+
+  switch (code) {
+    case 13:
+      enter = true;
+      break;
+    case 32:
+      space = true;
+      break;
+  }
+}
+
+function keyUp(e) {
+  var code = e.keyCode;
+
+  switch (code) {
+    case 13:
+      enter = false;
+    case 32:
+      space = false;
+  }
+}
+
 },{}]},{},[1]);
--- src/2018/04/src/js/mario.js
+++ src/2018/04/src/js/mario.js
@@ -7,4 +7,30 @@ let lastAnimationFrameTime = 0
 cxt.fillStyle = "rgb(255, 0, 0)"
 cxt.fillRect(0, 0, 640, 480)
 
+window.addEventListener('keydown', keyDown)
+window.addEventListener('keyup', keyUp)
 
+function keyDown(e) {
+  const code = e.keyCode
+
+  switch(code) {
+    case 13:
+      enter = true
+      break
+    case 32:
+      space = true
+      break
+  }
+}
+
+
+function keyUp(e) {
+  const code = e.keyCode
+
+  switch(code) {
+    case 13:
+      enter = false
+    case 32:
+      space = false
+  }
+}
