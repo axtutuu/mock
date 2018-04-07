af71d9c42787f00140a1f3dbf6ee410d564510fd
kawasaki-atsushi <kawasaki8910@gmail.com>
2018-04-07 15:52:14 +0900

    canvas base

---
+++ src/2018/04/src/js/mario.js
@@ -0,0 +1,10 @@
+console.log('mario')
+const canvas = document.querySelector('canvas')
+const cxt = canvas.getContext('2d')
+
+// animate()
+let lastAnimationFrameTime = 0
+cxt.fillStyle = "rgb(255, 0, 0)"
+cxt.fillRect(0, 0, 640, 480)
+
+
